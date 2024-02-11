115b8a572346bb304f7124b1489289a01c50788d
Elsayed Abdullah <sayedaladway@gmail.com>
2024-02-11 13:47:02 +0200

    install project packages, setup firebase, make firebase configuration and install routing

---
+++ src/router/index.tsx
@@ -0,0 +1,17 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+} from "react-router-dom";
+import RootLayout from "../components/RootLayout";
+import Homepage from "../components/Homepage";
+import Login from "../components/Login";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Homepage />}></Route>
+      <Route path="login" element={<Login />}></Route>
+    </Route>
+  )
+);
